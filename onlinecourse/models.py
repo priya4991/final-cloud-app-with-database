85ba145b69afec3fa34f54ad9caf4e25b5a590c4
Priyadarshini Dutta <priyadarshinidutta@192.168.1.134>
2022-05-10 23:32:26 +0100

    updates to admin and model.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -105,7 +105,7 @@ class Question(models.Model):
     # Foreign key to lesson
     # question text
     # question grade/mark
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Course, on_delete=models.CASCADE)
     question_text = models.TextField()
     grade = models.IntegerField(default=0)
 
